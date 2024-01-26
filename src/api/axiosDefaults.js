280358bed83b0a346653e8f05fec6d48f23cd2ab
Antonyeaster <>
2024-01-26 09:28:57 +0000

    Add Sign up form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://drf-api-antony-c121376a2219.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
